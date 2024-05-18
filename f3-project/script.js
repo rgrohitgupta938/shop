a5c711b98d5d60512facf192d7bf831ddfe6c93b
rgrohitgupta938 <49370602+rgrohitgupta938@users.noreply.github.com>
2024-05-18 17:47:38 +0530

    thired

--- f3-project/script.js
+++ f3-project/script.js
@@ -4,14 +4,14 @@ document.addEventListener("DOMContentLoaded", () => {
   console.log(btnGrp);
   let user = JSON.parse(localStorage.getItem("user") || "{}");
   if (Object.keys(user).length !== 0) {
-    let str = `<li><a href="../f3-project/index.html">Home</a></li><li><a href="../f3-project/shop/index.html">Shop</a></li>
-    <li><a href="../f3-project/profile/index.html">Profile</a></li><li><a href="../f3-project/cart/index.html">My Cart</a></li><li><a href="../f3-project/logout/index.html">Log Out</a></li>`;
+    let str = `<li><a href="/f3-project/index.html">Home</a></li><li><a href="/f3-project/shop/index.html">Shop</a></li>
+    <li><a href="/f3-project/profile/index.html">Profile</a></li><li><a href="/f3-project/cart/index.html">My Cart</a></li><li><a href="/f3-project/logout/index.html">Log Out</a></li>`;
     listHome.innerHTML = str;
     btnGrp.innerHTML = "";
   } else {
     let str = `<li><a href="../f3-project/index.html">Home</a></li>
-    <li><a href="../f3-project/login/index.html">Login</a></li>
-    <li><a href="../f3-project/signup/index.html">Signup</a></li>`;
+    <li><a href="/f3-project/login/index.html">Login</a></li>
+    <li><a href="/f3-project/signup/index.html">Signup</a></li>`;
     listHome.innerHTML = str;
     let str1 = `<button id="login">Login</button>
     <button id="signup">Sign Up</button>`;
@@ -20,10 +20,10 @@ document.addEventListener("DOMContentLoaded", () => {
 
   document.getElementById("login").onclick = () => {
     console.log("hello");
-    window.location.href = "../f3-project/login/index.html";
+    window.location.href = "/f3-project/login/index.html";
   };
 
   document.getElementById("signup").onclick = () => {
-    window.location.href = "../f3-project/signup/index.html";
+    window.location.href = "/f3-project/signup/index.html";
   };
 });
